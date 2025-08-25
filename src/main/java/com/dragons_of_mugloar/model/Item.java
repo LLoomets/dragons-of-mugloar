7479575cc245c42a4185f36e903871f61e719da2
Liisi Loomets <loomets@tlu.ee>
2025-08-25 13:38:53 +0300

    add item model

---
+++ src/main/java/com/dragons_of_mugloar/model/Item.java
@@ -0,0 +1,12 @@
+package com.dragons_of_mugloar.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item {
+    private String id;
+    private String name;
+    private int cost;
+}
